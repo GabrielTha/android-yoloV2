0e48a49fa1bdd6b37c70a0809ff4b9bca17fee37
Gabriel_Tha <gabriel.m.t@hotmail.com>
2020-01-13 13:39:40 -0300

    Initial Commit

---
+++ src/org/tensorflow/yolo/view/ImageSave.java
@@ -0,0 +1,57 @@
+package org.tensorflow.yolo.view;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class ImageSave {
+    private String NameOfFolder = "/A_IMG";
+    private String NameOfFile = "/A_IMG";
+    private String ImagePath;
+
+    public void SaveImage(Bitmap ImageToSave){
+        AbleToSave();
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath()+ NameOfFolder;
+        String CurrentDateAndTime= getCurrentDateAndTime();
+        File dir = new File(file_path);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        File file = new File(dir, NameOfFile +CurrentDateAndTime+ ".jpg");
+        try{
+            FileOutputStream fOut = new FileOutputStream(file);
+            ImageToSave.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+            AbleToSave();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String getCurrentDateAndTime() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String formattedDate = df.format(c.getTime());
+        return formattedDate;
+    }
+
+    private void AbleToSave() {
+        Toast.makeText(TheThis, "Picture saved be saved", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ src/org/tensorflow/yolo/view/VerifyClassification.java
@@ -0,0 +1,51 @@
+package org.tensorflow.yolo.view;
+
+import org.tensorflow.yolo.model.Recognition;
+
+import java.util.List;
+
+public class VerifyClassification {
+    public boolean verifyFossa(List<Recognition> lista){
+        Boolean flag = false;
+        if(lista.isEmpty()){
+            return(false);
+        }
+        for (int i = 0; i < lista.size(); i++){
+            Recognition reconhecido;
+            reconhecido = lista.get(i);
+            if (reconhecido.getTitle().equals("sofa")){
+                if (flag == false){
+                    flag = true;
+                }
+                else{
+                    return(true);
+                }
+            }
+        }
+        return(false);
+    }
+
+    public boolean verifyBoca(List<Recognition> lista){
+        if(lista.isEmpty()){
+            return(false);
+        }
+        for (int i = 0; i < lista.size(); i++){
+            Recognition reconhecido;
+            reconhecido = lista.get(i);
+            if (reconhecido.getTitle().equals("sofa")){
+                return(true);
+            }
+        }
+        return(false);
+    }
+
+    public String verifyList(List<Recognition> lista){
+//        if(this.verifyBoca(lista) && this.verifyFossa(lista)){
+//            return("Achou");
+//        }
+        if(this.verifyBoca(lista)){
+            return("Achou");
+        }
+        return ("Não achou");
+    }
+}
